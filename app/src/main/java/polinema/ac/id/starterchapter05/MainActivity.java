943891eed6cc46ea71349de98c513726e4f94e94
reihardana <reinaldohardana@gmail.com>
2019-09-30 17:03:45 +0700

    Tugas 100%

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 
+import polinema.ac.id.starterchapter05.fragments.Dips;
 import polinema.ac.id.starterchapter05.fragments.HandStand;
 import polinema.ac.id.starterchapter05.fragments.PushUp;
 
@@ -26,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handlerDips(View view) {
-
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.replace(R.id.fragment, new Dips(), "PUSH UP");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handlerHandstand(View view) {
