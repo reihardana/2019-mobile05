7b622e6cd6424eeb9d6553ed9306a4de90be6eb6
reihardana <reinaldohardana@gmail.com>
2019-09-30 16:54:06 +0700

    Tugas 80%

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -1,8 +1,13 @@
 package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.fragments.HandStand;
+import polinema.ac.id.starterchapter05.fragments.PushUp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlerPushUp(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.replace(R.id.fragment, new PushUp(), "PUSH UP");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    public void handlerDips(View view) {
+
+    }
+
+    public void handlerHandstand(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.replace(R.id.fragment, new HandStand(), "HAND STAND");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/HandStand.java
@@ -0,0 +1,25 @@
+package polinema.ac.id.starterchapter05.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import polinema.ac.id.starterchapter05.R;
+
+
+public class HandStand extends Fragment {
+    public HandStand() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hand_stand, container, false);
+    }
+}
